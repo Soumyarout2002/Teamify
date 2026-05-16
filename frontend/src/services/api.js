3ef7f576ae2fb27c918d1fdec1b68a4224f90592
Soumyarout2002 <soumyarout002@gmail.com>
2026-05-17 03:43:50 +0530

    Fix production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import toast from 'react-hot-toast'
 import { useAuthStore } from '../store/auth'
 
 const api = axios.create({
-  baseURL:         '/api',
+ baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 })
 
